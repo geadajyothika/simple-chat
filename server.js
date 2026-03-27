1736b4c36286357d293cfa24fe7a0c83045ef9d6
geada kishore <geadakishore123@gmail.com>
2026-03-27 19:16:39 +0530

    final deploy ready

--- server.js
+++ server.js
@@ -1,45 +1,75 @@
 const express = require("express");
 const http = require("http");
-const socketIO = require("socket.io");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server);
+const io = new Server(server);
 
+// Serve frontend
 app.use(express.static("public"));
 
-let users = [];
+let users = {};
+let messages = [];
 
+// SOCKET CONNECTION
 io.on("connection", (socket) => {
     console.log("User connected");
 
+    // JOIN
     socket.on("join", (username) => {
         socket.username = username;
-        users.push(username);
+        users[socket.id] = username;
 
-        io.emit("user list", users);
-        io.emit("system message", username + " joined");
-    });
+        // Send old messages
+        socket.emit("old messages", messages);
 
-    socket.on("chat message", (msg) => {
-        io.emit("chat message", {
-            user: socket.username,
-            text: msg,
-            time: new Date().toLocaleTimeString()
+        // Update users
+        const uniqueUsers = [...new Set(Object.values(users))];
+        io.emit("user list", uniqueUsers);
+
+        // Notify others
+        socket.broadcast.emit("message", {
+            type: "system",
+            text: username + " joined"
         });
     });
 
+    // CHAT MESSAGE
+    socket.on("chat message", (msg) => {
+        const data = {
+            type: "chat",
+            user: socket.username,
+            text: msg,
+            time: new Date().toLocaleTimeString()
+        };
+
+        messages.push(data);
+        io.emit("message", data);
+    });
+
+    // DISCONNECT
     socket.on("disconnect", () => {
-        users = users.filter(u => u !== socket.username);
+        const username = users[socket.id];
+        delete users[socket.id];
 
-        io.emit("user list", users);
+        const uniqueUsers = [...new Set(Object.values(users))];
+        io.emit("user list", uniqueUsers);
 
-        if (socket.username) {
-            io.emit("system message", socket.username + " left");
+        if (username) {
+            io.emit("message", {
+                type: "system",
+                text: username + " left"
+            });
         }
+
+        console.log("User disconnected");
     });
 });
 
-server.listen(3000, () => {
-    console.log("Server running on port 3000");
+// 🔥 IMPORTANT FOR DEPLOY
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
 });
